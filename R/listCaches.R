793f4cddd988b898f3d1162af7ce34b14d62d7d6
Vince Reuter <vince.reuter@gmail.com>
2017-08-15 10:24:05 -0400

    run document() for NAMESPACE to allow install; add return value

--- R/listCaches.R
+++ R/listCaches.R
@@ -3,6 +3,8 @@
 #' Lists any cache files in the cache directory.
 #'
 #' @param cacheSubDir Optional parameter to specify a subdirectory of the cache folder.
+#' @return \code{character} vector in which each element is the path to a file that 
+#'         represents an available cache (within \code{getOption("RCACHE.DIR")})
 #' @export
 #' @example
 #' R/examples/example.R
