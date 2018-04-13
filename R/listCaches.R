5bc2ba79c09412d3c2aa47be8bdacdb375cf4252
Vince Reuter <vince.reuter@gmail.com>
2018-04-13 09:20:52 -0400

    only list files that appear to be caches

--- R/listCaches.R
+++ R/listCaches.R
@@ -9,6 +9,7 @@
 #' @example
 #' R/examples/example.R
 listCaches = function(cacheSubDir="") {
-	list.files(paste0(getOption("RCACHE.DIR"), cacheSubDir))
+	cacheDirFiles = list.files(paste0(getOption("RCACHE.DIR"), cacheSubDir))
+	cacheDirFiles[which(sapply(cacheDirFiles, function(f) endsWith(f, ".RData")))]
 }
 
